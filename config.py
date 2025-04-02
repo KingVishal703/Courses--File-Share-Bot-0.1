5651dde40e98e937a4aeb37f03791b3e44af07b3
KingVishal703 <vishal21wankhede@gmail.com>
2025-04-02 10:53:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
         return default
 
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "25452590"))
+API_HASH = environ.get("API_HASH", "7ccce409c7280e0153521df4458df7e3")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # force sub cahnel id
